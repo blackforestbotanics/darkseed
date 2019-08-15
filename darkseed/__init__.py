98e8dcf8916f72791f0b16f881892308d3613d7e
Tom <tom@blackforestbotanics.com>
2019-08-15 07:56:39 -0700

    provide exports for darkseed usage via api

--- darkseed/__init__.py
+++ darkseed/__init__.py
@@ -1,6 +1,11 @@
 """
 all of darkseed must be import safe. autodoc runs on all members.
+
+Top-level objects and functions are offered here for using Darkseed as a
+library.
 """
+
+from .utils import release
 
 def get_path():
     """get the theme path"""
